ddce9386b26f8257189c1bb0e6b70ceef79c9356
etouch-solutions <etouchindiasolutions@gmail.com>
2025-06-11 12:56:33 +0530

    Initial commit

---
+++ include/footer.php
@@ -0,0 +1,44 @@
+
+ <footer class="site-footer">
+  <div class="footer-container">
+    <div class="footer-col">
+      <h4>About Us</h4>
+      <p>Welcome to Sri Vengadeswara Glass House, Tamil Nadu’s trusted name in premium glass, UPVC, hardware, and interior solutions.</p>
+    </div>
+
+    <div class="footer-col">
+      <h4>Quick Links</h4>
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="product.html">Products</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </div>
+
+    <div class="footer-col">
+      <h4>Contact Info</h4>
+      <p>Email: <a href="mailto:srivenkateswarakrr@gmail.com">srivenkateswarakrr@gmail.com </a></p>
+      <p>Phone: +91 97919 17872</p>
+      <p>Phone: +91  99445 67842</p>
+      <p>Address: No.87/G, திரு.வி.க ரோடு (உழவர் சந்தை எதிரில்), கரூர் – 639001</p>
+    </div>
+
+    <div class="footer-col">
+      <h4>Follow Us</h4>
+      <ul class="social-links">
+      <li><a href="tel:+919791917872"><i class="fas fa-phone-alt"></i> Call us</a></li>
+  <li><a href="https://www.facebook.com/people/Welcome-to-sri-vengateswara-Glass-karur-9791917872/100063898004553/"><i class="fab fa-facebook-f"></i> Facebook</a></li>
+  <li><a href="https://www.instagram.com/srivenkateswaraglasshouse/"><i class="fab fa-instagram"></i> Instagram</a></li>
+  <li><a href="https://wa.me/919791917872?text=Hello%20Sri%20Venkateswara%20Glass%20House%2C%20I%20would%20like%20to%20know%20more%20about%20your%20products."><i class="fab fa-whatsapp"></i> WhatsApp</a></li>
+  
+
+
+      </ul>
+    </div>
+  </div>
+
+  <div class="footer-bottom">
+    <p>&copy; 2025 sri venkateshwara glass house. All rights reserved. powered by <a style="text-decoration: none; color: rgb(200, 255, 0); font-weight: 900;" href="https://etouchsolutions.in/">eTouch</a></p>
+  </div>
+</footer>
---
+++ include/header.php
@@ -0,0 +1,15 @@
+ <header>
+    <div class="logo"><img class="logo-img" src="img/Untitled (500 x 200 px).png" alt=""></div>
+
+    <div class="menu-icon" onclick="toggleMenu()">☰</div>
+
+    <nav id="navbar">
+      <a href="index.html">Home</a>
+      <a href="about.html">About us</a> 
+      <a href="product.html">Products</a>
+      <a href="contact.html">Contact us</a>
+    </nav>
+
+   <a style="text-decoration: none;" href="expo.html"> <button style="background-color: #09026b;" class="cta-btn " >Get Started</button></a>
+  </header>
+ 
