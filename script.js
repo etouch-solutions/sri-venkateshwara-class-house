f826c9f332510c4aac7554722f7ead3acc548911
etouch-solutions <etouchindiasolutions@gmail.com>
2025-06-13 12:48:29 +0530

    Initial commit

--- script.js
+++ script.js
@@ -2,17 +2,4 @@
       document.getElementById('navbar').classList.toggle('active');
     }
 
-
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-      }
-    });
-  }, {
-    threshold: 0.1 // Adjust if you want it to trigger earlier/later
-  });
-
-  document.querySelectorAll('.section').forEach(section => {
-    observer.observe(section);
-  });
+ 
